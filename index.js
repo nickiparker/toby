0bbf47e322c26ed3e00fe6f55540853a810f361c
Nicki Parker <NP204409@falmouth.ac.uk>
2017-07-08 21:28:17 +0100

    Instantiate new clarifai app

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+// instantiate a new Clarifai app passing in your clientId and clientSecret
+var app = new Clarifai.App(
+  CLIENT_ID,
+  CLIENT_SECRET
+);
+
 function run(imgurl) {
   document.getElementById("image_given").src=imgurl;
 }
