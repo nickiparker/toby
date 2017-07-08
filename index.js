4ad4c4dbf4587e1eabc1efcfbcd8435a4050a07f
Nicki Parker <NP204409@falmouth.ac.uk>
2017-07-08 21:40:44 +0100

    Add clarifai predict model and parse response to return top 5 tags

--- index.js
+++ index.js
@@ -4,6 +4,29 @@ var app = new Clarifai.App(
   CLIENT_SECRET
 );
 
+function postImage(imgurl) {
+
+  app.models.predict(Clarifai.GENERAL_MODEL, imgurl).then(
+    function(response) {
+      parseResponse(response);  
+    },
+    function(err) {
+      console.error(err);
+    }
+  );
+}
+
+function parseResponse(response) {
+  var tags = [];
+    var results = response["outputs"][0]["data"]["concepts"];
+    results.forEach(function(result) {
+      tags.push(result["name"]);
+    });
+  var first_five_tags = tags.slice(0, 5);
+  console.log(first_five_tags);
+}
+
 function run(imgurl) {
+  postImage(imgurl);
   document.getElementById("image_given").src=imgurl;
 }
